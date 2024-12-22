6574eb3c6ee35064e0858983f0164ee1f30cdbc6
Raj Bhadani <rajbhadani9897@gmail.com>
2024-12-22 19:47:22 +0530

    Add files via upload

---
+++ arrow_function.js
@@ -0,0 +1,16 @@
+// var hello = () =>console.log('Hello World')
+
+// hello()
+
+// var sum = (a,b) => a+b
+
+// console.log(sum(5,9)) 
+
+// var process = (a,b) =>{
+//     return a*b 
+// }
+// console.log(process(5,6))
+
+(function(){
+    console.log("Hello World")
+})() 
